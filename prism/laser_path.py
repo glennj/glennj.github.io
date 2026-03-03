e081b00d16b3a7a95f1279bea9659cac583c6bf9
Glenn Jackman <glenn.jackman@gmail.com>
2026-03-03 14:55:49 -0500

    align the prism chart's points and arrows

--- prism/laser_path.py
+++ prism/laser_path.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 style = {'dark': 'dark_background', 'light': 'seaborn-v0_8-muted'}
-theme = 'light'
+theme = 'dark'
 
 # color styles
 #plt.style.use('dark_background')
@@ -22,29 +22,38 @@ p4 = (20, 0)
 # 1. Plot the points (Red dots)
 points = [p0, p1, p2, p3, p4]
 labels = ['Origin', 'Prism 1', 'Prism 2', 'Prism 3', 'Prism 4']
-
-for p, label in zip(points, labels):
-    ax.plot(p[0], p[1], 'ro', markersize=8)
-    ax.text(p[0], p[1] + 0.8, f'{label} {p}', ha='center', fontsize=10, fontweight='bold')
+colors = ['blue', 'green', 'red', 'orange', 'grey']
+label_coords = [
+    (0, -0.8),  # Origin
+    (10, 10.5),    # Prism 1
+    (10, -0.8),    # Prism 2
+    (30, 9.2),    # Prism 3
+    (20, -0.8)     # Prism 4
+]
 
 # 2. Draw the arrows with specific colors
 # Arrow: (0, 0) to Point 2 (Blue)
 ax.annotate('', xy=p2, xytext=(0, 0),
-            arrowprops=dict(facecolor='blue', edgecolor='blue', shrink=0, width=1.5, headwidth=8))
+            arrowprops=dict(facecolor='blue', edgecolor='blue', shrink=0.01, width=1.5, headwidth=8))
 
 # Arrow: Point 2 to Point 1 (Green)
 ax.annotate('', xy=p1, xytext=p2,
-            arrowprops=dict(facecolor='green', edgecolor='green', shrink=0, width=1.5, headwidth=8))
+            arrowprops=dict(facecolor='green', edgecolor='green', shrink=0.01, width=1.5, headwidth=8))
 
 # Arrow: Point 1 to Point 3 (Red)
 ax.annotate('', xy=p3, xytext=p1,
-            arrowprops=dict(facecolor='red', edgecolor='red', shrink=0, width=1.5, headwidth=8))
+            arrowprops=dict(facecolor='red', edgecolor='red', shrink=0.01, width=1.5, headwidth=8))
 
 # Arrow: Point 3 at 45 degrees (Orange)
 # We calculate a point at (30+7, 10+7) to ensure a perfect 45-degree slope
 end_p3_arrow = (p3[0] + 7, p3[1] + 7)
 ax.annotate('', xy=end_p3_arrow, xytext=p3,
-            arrowprops=dict(facecolor='orange', edgecolor='orange', shrink=0, width=1.5, headwidth=8))
+            arrowprops=dict(facecolor='orange', edgecolor='orange', shrink=5, width=1.5, headwidth=8))
+
+# now add the points and labels on top of the arrows
+for p, label, color, label_coord in zip(points, labels, colors, label_coords):
+    ax.plot(p[0], p[1], 'ro', markersize=10, markerfacecolor=color, markeredgecolor=color)
+    ax.text(label_coord[0], label_coord[1], label, ha='center', fontsize=10, fontweight='bold')
 
 # 3. Configure the Grid and Axes
 ax.set_xlim(-2, 40)
